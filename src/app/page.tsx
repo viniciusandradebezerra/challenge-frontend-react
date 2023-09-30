8222de03d6f2d07cffa02413668453c91dccce15
Vinicius Andrade <46060247+vinippts@users.noreply.github.com>
2023-09-30 20:02:18 -0300

    chore(products.db): update products.db binary file style(page.tsx): fix formatting and add missing semicolons for better code readability feat(Payment.api.ts): update apiUrl variable to use double quotes for consistency feat(Payment.api.ts): add error handling and logging for payment checkout process refactor(index.ts): fix import formatting for better code organization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,26 @@
-
-'use client'
+"use client";
 import { useQuery } from "@tanstack/react-query";
 import { TProductDto } from "@types";
 import { ProductsApi } from "@services";
 import { Typography } from "@mui/material";
 import { ProductTable } from "@components";
 
-
 export default function Home() {
-
   const { data, isLoading, isError } = useQuery<TProductDto[]>({
     queryKey: ["products"],
     queryFn: () => ProductsApi.getAllProducts(),
     staleTime: 5 * 1000,
   });
 
-  if(isLoading) return <Typography>Loading...</Typography>
+  console.log(data);
 
-  if(isError) return <Typography>Client or Sever Error...</Typography>
+  if (isLoading) return <Typography>Loading...</Typography>;
+
+  if (isError) return <Typography>Client or Sever Error...</Typography>;
 
   return (
     <>
-        <ProductTable products={data} />
+      <ProductTable products={data} />
     </>
   );
 }
--- src/app/services/api/payment/Payment.api.ts
+++ src/app/services/api/payment/Payment.api.ts
@@ -2,38 +2,36 @@ import { ICheckoutBody } from "@types";
 import { loadStripe } from "@stripe/stripe-js";
 import Stripe from "stripe";
 
-const apiUrl = 'http://localhost:3000/api/payment';
-
+const apiUrl = "http://localhost:3000/api/payment";
 
 const checkout = async (body: ICheckoutBody) => {
-    const STRIPE_PK = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!;
-    const stripe = await loadStripe(STRIPE_PK);
+  const STRIPE_PK = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!;
+  const stripe = await loadStripe(STRIPE_PK);
 
-    try {
-        const response = await fetch(`${apiUrl}`, {
-            method: "post",
-            body: JSON.stringify(body, null),
-            headers: {
-                "content-type": "application/json",
-            },
-        });
-        if (!response.ok) {
-            throw new Error("Failed to create Stripe Checkout session.");
-        }
-        const data = (await response.json()) as Stripe.Checkout.Session;
-        const sessionId = data.id;
-
-        if (sessionId) {
-            stripe?.redirectToCheckout({ sessionId });
-        } else {
-            throw new Error("Session ID is missing in the API response.");
-        }
-    } catch (error) {
-        console.error("Payment error:", error);
+  try {
+    const response = await fetch(apiUrl, {
+      method: "post",
+      body: JSON.stringify(body, null),
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Failed to create Stripe Checkout session.");
     }
+    const data = (await response.json()) as Stripe.Checkout.Session;
+    const sessionId = data.id;
+
+    if (sessionId) {
+      stripe?.redirectToCheckout({ sessionId });
+    } else {
+      throw new Error("Session ID is missing in the API response.");
+    }
+  } catch (error) {
+    console.error("Payment error:", error);
+  }
 };
 
-
 export const PaymentApi = {
-    checkout,
+  checkout,
 };
